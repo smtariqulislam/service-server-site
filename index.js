5674f860a77ec1c743b9722b5a55ba2c488ab60b
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-11-10 10:43:08 +0600

    single services api make for dynamic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 
@@ -24,7 +24,6 @@ app.listen(port, () => {
 
 
 //database
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.781yldu.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, {
@@ -32,8 +31,35 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+
+    try{
+        const serviceCollection = client.db('Advisor').collection('services');
+        app.get('/services', async(req,res)=>{
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services);
+
+        });
+
+        app.get('/services/:id', async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id:ObjectId(id)};
+          const service = await serviceCollection.findOne(query);
+          res.send(service)
+
+
+        })
+
+
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(err =>console.error(err));
+
